d7015e945211076784b3ba8f0f78eee232772630
Danilo <danilo.nascimento2@hotmail.com>
2023-04-03 11:32:59 -0300

    Tailwind config and navbar components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/navbar'
 
 const font = Nunito ({
   subsets: ["latin"],
@@ -13,9 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={font.className}>
-        Airbnb
-      </main>
+      <Navbar />
     </>
   )
 }
